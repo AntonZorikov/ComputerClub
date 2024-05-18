a42ec210a803f9c45db43a6d8f1e246733e951ac
AntonZorikov <antonzorikov@mail.ru>
2024-05-18 23:49:30 +0300

    some changes

--- tests/test.cpp
+++ tests/test.cpp
@@ -64,7 +64,7 @@ TEST(ClubTest, positive_1) {
     club.run();
     std::string output = catcher.getOutput();
 
-std::string expectedOutput = "09:00\n08:48 1 client1\n08:48 13 NotOpenYet\n09:41 1 client1\n09:48 1 client2\n09:52 3 client1\n09:52 13 ICanWaitNoLonger!\n09:54 2 client1 1\n10:25 2 client2 2\n10:58 1 client3\n10:59 2 client3 3\n11:30 1 client4\n11:35 2 client4 2\n11:35 13 PlaceIsBusy\n11:45 3 client4\n12:33 4 client1\n12:33 12 client4 1\n12:43 4 client2\n15:52 4 client4\n19:00 11 client3\n19:00\n1 70 05:58\n2 30 02:18\n3 90 08:01\n";
+    std::string expectedOutput = "09:00\n08:48 1 client1\n08:48 13 NotOpenYet\n09:41 1 client1\n09:48 1 client2\n09:52 3 client1\n09:52 13 ICanWaitNoLonger!\n09:54 2 client1 1\n10:25 2 client2 2\n10:58 1 client3\n10:59 2 client3 3\n11:30 1 client4\n11:35 2 client4 2\n11:35 13 PlaceIsBusy\n11:45 3 client4\n12:33 4 client1\n12:33 12 client4 1\n12:43 4 client2\n15:52 4 client4\n19:00 11 client3\n19:00\n1 70 05:58\n2 30 02:18\n3 90 08:01\n";
 
     EXPECT_EQ(output, expectedOutput);
 }
@@ -84,7 +84,7 @@ TEST(ClubTest, positive_2) {
     club.run();
     std::string output = catcher.getOutput();
 
-std::string expectedOutput = "09:00\n19:00\n1 0 00:00\n";
+    std::string expectedOutput = "09:00\n19:00\n1 0 00:00\n";
 
     EXPECT_EQ(output, expectedOutput);
 }
@@ -105,7 +105,7 @@ TEST(ClubTest, positive_3) {
     club.run();
     std::string output = catcher.getOutput();
 
-std::string expectedOutput = "09:00\n10:00 1 client1\n11:00 2 client1 1\n19:00 11 client1\n19:00\n1 80 08:00\n";
+    std::string expectedOutput = "09:00\n10:00 1 client1\n11:00 2 client1 1\n19:00 11 client1\n19:00\n1 80 08:00\n";
 
     EXPECT_EQ(output, expectedOutput);
 }
@@ -126,7 +126,7 @@ TEST(ClubTest, negative_1) {
     club.run();
     std::string output = catcher.getOutput();
 
-std::string expectedOutput = "09::00 19:00";
+    std::string expectedOutput = "09::00 19:00";
 
     EXPECT_EQ(output, expectedOutput);
 }
@@ -147,7 +147,7 @@ TEST(ClubTest, negative_2) {
     club.run();
     std::string output = catcher.getOutput();
 
-std::string expectedOutput = "10:00 10 client1";
+    std::string expectedOutput = "10:00 10 client1";
 
     EXPECT_EQ(output, expectedOutput);
 }
